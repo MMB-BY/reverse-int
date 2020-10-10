50673db2e602a3914d4bea725eb286c2e4cc6dd5
MMB-BY <mmbmainp@gmail.com>
2020-10-10 18:27:14 +0300

    init: started reversed int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n *= -1;
+    }
+  let buf = n.toString().split('');
+  let res = [];
+  for (let i = buf.length - 1; i >= 0 ; i--){
+    res.push(buf[i]);
+  }
+  res = res.join('');
+  return +res;
 }
